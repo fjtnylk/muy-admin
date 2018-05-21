cac342d8afece9eea088acee07fec96adb0d5c7f
yanglikai <yanglikai@yonghui.cn>
2018-05-21 16:30:10 +0800

    #yanglikai #2018=05-21 #muy-web-admin应用开发

---
+++ muy-web/muy-web-admin/src/main/java/com/muy/admin/MuyWebAdminApplication.java
@@ -0,0 +1,12 @@
+package com.muy.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuyWebAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuyWebAdminApplication.class, args);
+	}
+}
